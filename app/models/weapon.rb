dbe6edcd4301ac99ff1d8caa237a6d379f2e8b1c
Tom Levy <tom@solveit.fr>
2020-02-17 18:43:58 +0100

    commit

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -5,9 +5,3 @@ class Weapon < ApplicationRecord
   validates :price, :name, :category, :localisation, presence: true
 end
 
-# bazooka = Weapon.new
-# bazooka.name = "bazooka"
-# bazooka.price = 2000
-# bazooka.category = "grosse armes"
-# bazooka.localisation = "paris 17"
-# bazooka.user = paul
