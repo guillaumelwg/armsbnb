a4ba7d461e162d64d9d502f1658c4cb7ace57b05
Olivier Godard <g.olivier.78@gmail.com>
2020-02-20 18:07:09 +0100

    home partie 1 sans les 3 cards

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :weapons
 
-  resources :weapons, only: [ :index, :new ] do
+  resources :weapons do
     resources :bookings, only: [ :create ]
   end
 
