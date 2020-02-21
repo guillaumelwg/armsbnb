23dea314035e1d3e63f632931b87b189e8c85e58
Guillaume Protard <guillaumeprotard@gmail.com>
2020-02-21 12:46:36 +0100

    fix mapbox youpi

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,6 +4,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 const mapElement = document.getElementById('map');
 
 const buildMap = () => {
+  console.log('coucou')
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
@@ -25,7 +26,6 @@ const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0});
-  console.log(bounds)
 };
 
 const initMapbox = () => {
